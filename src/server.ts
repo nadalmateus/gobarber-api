bffc9c87fc92065d1bbe30fa92cdfa9da56d32d6
Mateus Nadal <nlmatt07@hotmail.com>
2020-11-19 17:41:54 -0300

    Rusky Configuration

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,13 @@ import routes from './routes';
 const app = express();
 
 app.use(routes);
-
 app.use(express.json());
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+  return response.json({ message: 'Hello World!!!' });
 });
 
 const port = 3333;
 app.listen(3333, () => {
-  return console.log(`Server is listening on port ${port}`);
+  return console.log(`Server running on port ${port}`);
 });
